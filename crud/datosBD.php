020a250e0913333b6776755b38b3737fccf5739b
lenovo <yollisoto29@gmail.com>
2024-08-05 21:14:30 -0600

    actualizar

--- crud/datosBD.php
+++ crud/datosBD.php
@@ -1,7 +1,7 @@
 <?php
 define('servidor','localhost');  // El servidor MySQL, típicamente 'localhost'
 define('usuario','root');        // El nombre de usuario de tu base de datos MySQL
-define('contra',''); // La contraseña de tu base de datos MySQL
+define('contra','root'); // La contraseña de tu base de datos MySQL
 define('bd','sup_nat');          // El nombre de la base de datos MySQL
 define('puerto','3306');         // El puerto por defecto de MySQL es 3306
 ?>
